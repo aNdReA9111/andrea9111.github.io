d93ffa8e10b21b643f7a7d63a3f6fa6e5788c80f
aNdReA9111 <andrea.fiorellino@studio.unibo.it>
2025-11-07 03:59:15 +0100

    fix email

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,5 +1,5 @@
 export const contact = {
-  email: 'andrea.fiorellino@studio',
+  email: 'andrea.fiorellino@studio.unibo.it',
   github: 'https://github.com/aNdReA9111',
   // linkedin: 'https://linkedin.com/in/yourprofile',
 };
